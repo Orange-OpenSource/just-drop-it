6a80a95180047ad2a114c8d5bdc2741dbb60e760
Arnaud Ruffin <arnaud.ruffin@orange.com>
2015-09-29 12:36:30 +0200

    fusion en cours

--- client/jdropit-send.js
+++ client/jdropit-send.js
@@ -8,20 +8,14 @@ SenderHandler.prototype = {
     constructor: SenderHandler,
 
     init: function (isLocal) {
-        this.bytesPerChunk = 8 * 1024 * 1024; // 1MB chunk sizes.
         this.readWriteOpts = {highWaterMark: Math.pow(2, 21)};// 2 pow 10 = 1024
-        this.minChannel = 5;
-        this.nbPacket = 0;
-        this.fileSlices = [];
         this.receiverInfos = [];
-
         var that = this;
 
         //init du socket vers le serveur
         this.socketParams = {
             query: 'role=sender',
-            'multiplex': false /*so that calling a new socket doesn't return the previous cached one */,
-            transports:  [ 'websocket', 'polling' ]
+            transports: ['websocket', 'polling']
         };
 
         if (!isLocal)//restriction on OPENSHIFT
@@ -32,8 +26,8 @@ SenderHandler.prototype = {
             displayError("Error: " + errorMsg);
         });
 
-        this.socket.on('connect', function() {
-            console.log(this.id+" - "+this.io.engine.transport.name);
+        this.socket.on('connect', function () {
+            console.log(this.id + " - " + this.io.engine.transport.name);
         });
 
         this.socket.on('server_receive_url_ready', function (url) {
@@ -47,81 +41,67 @@ SenderHandler.prototype = {
             $('#copyLinkContainer').hide();
             $('#transfertMessage').html("Transfert in progress...");
 
-            /*****TODO AVANT CA RESSEMBLAIT A CA
-            var row = $("<div>", {id : getRowId(receiverId)}).addClass("row");
-            row.append($("<div>").addClass("col-xs-4").addClass("receiver-label").append($("<p>").html(receiverLabel)));
-            var transferProgressBar = $("<div>", {id : getTransfertBarId(receiverId), role : "progressbar", "aria-valuenow": "0", "aria-valuemin" : "0", "aria-valuemax" : "1000", style : "min-width: 2em;"}).addClass("progress-bar progress-bar-striped active").html("0 %");
-            row.append($("<div>", {id : getTransfertBarContainerId(receiverId)}).addClass("col-xs-7").append(transferProgressBar));
-            var linkRemove = $("<a>", {href : "#"}).append($("<span>").addClass("glyphicon glyphicon-remove"));
-            linkRemove.on("click", function(e){
-                e.preventDefault();
-                row.hide();
-            });
-            row.append($("<div>", {id : "transfert-"+receiverId+"-remove", hidden : "true"}).addClass("col-xs-1").append(linkRemove));
-            transferContainer.append(row);
-
-            /***/
-
             //init container
-            var transferContainer = $('#transferContainer');
-            transferContainer.show();
+            /**TODO c'était bien, clone
+             *  var transferContainer = $('#transferContainer');
+             transferContainer.show();
 
-            var rowReceiverTemplate = $("#rowReceiverTemplate");
-            var newRow = rowReceiverTemplate.clone();
-            newRow.attr("id", "receiver-col-" + receiverId);
-            newRow.children(".col-xs-4").append($("<p>").html(receiverLabel));
-            var linkContainer = newRow.children(".col-xs-1");
-            var linkRemove = linkContainer.children("a");
-            linkRemove.on("click", function (e) {
+             var rowReceiverTemplate = $("#rowReceiverTemplate");
+             var newRow = rowReceiverTemplate.clone();
+             newRow.attr("id", "receiver-col-" + receiverId);
+             newRow.children(".col-xs-4").append($("<p>").html(receiverLabel));
+             var linkContainer = newRow.children(".col-xs-1");
+             var linkRemove = linkContainer.children("a");
+             linkRemove.on("click", function (e) {
                 e.preventDefault();
                 newRow.hide();
             });
 
-            var pbContainer = newRow.children(".col-xs-7");
+             var pbContainer = newRow.children(".col-xs-7");
 
-            var templateBarContainer = rowReceiverTemplate.children(".col-xs-7");
-            transferContainer.append(newRow);
-
-
+             var templateBarContainer = rowReceiverTemplate.children(".col-xs-7");
+             transferContainer.append(newRow);**/
 
 
-           that.receiverInfos[receiverId] = new ReceiverInfo(receiverLabel, pbContainer, clusterProgressBar, linkContainer, that.nbPacket, sockets);
 
+            var transferContainer = $('#transferContainer');
+            var row = $("<div>", {id: "receiver-col-" + receiverId}).addClass("row");
+            row.append($("<div>").addClass("col-xs-4").addClass("receiver-label").append($("<p>").html(receiverLabel)));
+            var transferProgressBar = $("<div>", {
+                id: "transfertProgresssBar-" + receiverId,
+                role: "progressbar",
+                "aria-valuenow": "0",
+                "aria-valuemin": "0",
+                "aria-valuemax": "1000",
+                style: "min-width: 2em;"
+            }).addClass("progress-bar progress-bar-striped active").html("0 %");
+            var pbContainer = $("<div>", {id: "transfertProgresssBarContainer-" + receiverId});
+            row.append(pbContainer).addClass("col-xs-7").append(transferProgressBar);
+            var linkRemove = $("<a>", {href: "#"}).append($("<span>").addClass("glyphicon glyphicon-remove"));
+            var linkContainer = $("<div>", {id: "transfert-" + receiverId + "-remove", hidden: "true"});
+            linkRemove.on("click", function (e) {
+                e.preventDefault();
+                row.hide();
+            });
+            row.append(linkContainer).addClass("col-xs-1").append(linkRemove);
+            transferContainer.append(row);
+
+
+            that.receiverInfos[receiverId] = new ReceiverInfo(receiverLabel, pbContainer, transferProgressBar, linkContainer);
+
+            that.startUpload(receiverId);
 
         });
 
-        this.socket.on('server_receiver_left', function (receiverId, receiverLabel) {
+        this.socket.on('server_receiver_left', function (receiverId) {
             var receiverInfo = that.receiverInfos[receiverId];
             var progressBarContainer = receiverInfo.progressBarContainer;
             progressBarContainer.empty();
             progressBarContainer.append($("<p>").addClass("text-error").html("Receiver left before end of transfer"));
             receiverInfo.removeLinkContainer.show();
-            receiverInfo.dataSocketPool.closeAll();
-            if (typeof receiverLabel != "undefined" && receiverLabel.length != 0) {
-                receiverLabel = receiverLabel + " "
-            }
-            jdNotif.notify("Something is wrong", "Apparently your friend " + receiverLabel + "left before the transfer was over");
+            jdNotif.notify("Something is wrong", "Apparently your friend " + receiverInfo.label + "left before the transfer was over");
 
         });
-
-        this.socket.on('rcv_packet_received', function (receiverId, packetIndex) { //TODO add data-socketId in args
-            that.receiverInfos[receiverId].progressBar.fillCluster(packetIndex);
-            var totalReceived = that.receiverInfos[receiverId].addPacketReceived(packetIndex);
-            that.receiverInfos[receiverId].dataSocketPool.release(packetIndex);
-            console.log("totalReceived="+totalReceived);
-            if (totalReceived == that.fileSlices.length) {
-                that.transferComplete(receiverId);
-            } else {
-                that.sendNextPacket(receiverId);
-            }
-        });
-
-        this.socket.on('rcv_packet_failed', function (receiverId, packetIndex) {
-            //TODO UI feedback on network troubles?
-            that.receiverInfos[receiverId].dataSocketPool.release(packetIndex);
-            that.sendPacket(packetIndex, receiverId)
-        });
-
     },
 
     fileIsReady: function (fileToTransfert) {
@@ -129,16 +109,9 @@ SenderHandler.prototype = {
             fileToTransfert.size > 1024 ? Math.round(fileToTransfert.size / 1024) + " Ko" : fileToTransfert.size + " o";
         $('#filename').html(fileToTransfert.name + " ( " + sizeDisplay + " )");
 
-        var SIZE = fileToTransfert.size;
-        var start = 0;
-        var end = this.bytesPerChunk;
-        this.nbPacket = 0;
-        while (start < SIZE) {
-            this.fileSlices.push(fileToTransfert.slice(start, end));
-            start = end;
-            end = start + this.bytesPerChunk;
-            this.nbPacket++;
-        }
+        this.fileToTransfer = fileToTransfert;
+
+
         this.socket.emit('snd_file_ready', {
             size: fileToTransfert.size,
             name: fileToTransfert.name,
@@ -149,28 +122,36 @@ SenderHandler.prototype = {
         $('#selectFileContainer').hide(500);
     },
 
-    sendNextPacket: function (receiverId) {
-        var nextPacketIndex = this.receiverInfos[receiverId].getNextPacketIndex();
-        if(typeof nextPacketIndex != "undefined")
-            this.sendPacket(nextPacketIndex, receiverId);
+    startUpload: function (receiverId) {
+        console.log(this.fileToTransfer);
+        var transferProgressBar = this.receiverInfos[receiverId].progressBar;
+
+        $('#transfertMessage').html("Transfert in progress...");
+        var stream = ss.createStream(this.readWriteOpts);
+
+        // upload a file to the server.
+        ss(this.socket).emit('snd_send_file', stream, receiverId);
+
+        var blobStream = ss.createBlobReadStream(this.fileToTransfer, this.readWriteOpts);
+        var size = 0;
+        var that = this;
+        blobStream.on('data', function (chunk) {
+            size += chunk.length;
+            var progress = Math.floor(size / file.size * 100);
+
+            //update progress bar
+            transferProgressBar.attr('aria-valuenow', progress);
+            transferProgressBar.width(progress + '%');
+            transferProgressBar.html(progress + '%');
+
+            if (progress >= 100) {
+                that.transferComplete(receiverId);
+            }
+        });
+
+        blobStream.pipe(stream);
     },
 
-    sendPacket: function (packetIndex, receiverId) {
-        var fileSlice = this.fileSlices[packetIndex];
-        if (typeof fileSlice == "undefined") {
-            console.error("Unknown packet " + packetIndex + "/" + this.nbPacket);
-        } else {
-            console.log("sendPacket - send packet " + (packetIndex+1) + "/" + this.nbPacket);
-            var stream = ss.createStream(this.readWriteOpts);
-            //retrieve free socket
-            var dataSocket=this.receiverInfos[receiverId].dataSocketPool.pull(packetIndex);
-
-            // upload a file to the server.
-            ss(dataSocket).emit('snd_send_packet', this.socket.id, stream, receiverId, packetIndex, fileSlice.size);
-            var blobStream = ss.createBlobReadStream(fileSlice, this.readWriteOpts);
-            blobStream.pipe(stream);
-        }
-    },
 
     transferComplete: function (receiverId) {
         console.log("transfer_complete - " + receiverId);
@@ -179,14 +160,12 @@ SenderHandler.prototype = {
         progressBarContainer.empty();
         progressBarContainer.append($("<p>").addClass("text-success").html("File sent"));
         receiverInfo.removeLinkContainer.show();
-        receiverInfo.dataSocketPool.closeAll();
 
         jdNotif.notify("Transfer complete", "your friend " + this.receiverInfos[receiverId].label + "correctly received your file");
 
         this.socket.emit("snd_transfer_complete", receiverId);
     }
 };
-
 
 
 function sendFile(isLocal) {
@@ -220,9 +199,6 @@ function sendFile(isLocal) {
         return false;
     });
 
-
-
-
     //soumission du fichier via formulaire
     $('#file').change(function (e) {
         senderHandler.fileIsReady(e.target.files[0]);
@@ -241,125 +217,6 @@ function sendFile(isLocal) {
     });
 
 
-    function getRowId(receiverId){
-        return "receiver-col-"+receiverId;
-    }
-
-
-    function getTransfertBarId(receiverId){
-        return "transfertProgresssBar-"+receiverId;
-    }
-
-    function getTransfertBarContainerId(receiverId){
-        return "transfertProgresssBarContainer-"+receiverId;
-    }
-
-    socket.on('receiver_ready', function (receiverId, receiverLabel) {
-        $('#copyLinkContainer').hide(500);
-        $('#transferContainer').show(500);
-
-        //init container
-
-        var transferContainer = $('#transferContainer');
-        var row = $("<div>", {id : getRowId(receiverId)}).addClass("row");
-        row.append($("<div>").addClass("col-xs-4").addClass("receiver-label").append($("<p>").html(receiverLabel)));
-        var transferProgressBar = $("<div>", {id : getTransfertBarId(receiverId), role : "progressbar", "aria-valuenow": "0", "aria-valuemin" : "0", "aria-valuemax" : "1000", style : "min-width: 2em;"}).addClass("progress-bar progress-bar-striped active").html("0 %");
-        row.append($("<div>", {id : getTransfertBarContainerId(receiverId)}).addClass("col-xs-7").append(transferProgressBar));
-        var linkRemove = $("<a>", {href : "#"}).append($("<span>").addClass("glyphicon glyphicon-remove"));
-        linkRemove.on("click", function(e){
-            e.preventDefault();
-            row.hide();
-        });
-        row.append($("<div>", {id : "transfert-"+receiverId+"-remove", hidden : "true"}).addClass("col-xs-1").append(linkRemove));
-        transferContainer.append(row);
-
-        startUpload(fileToTransfert, receiverId,transferProgressBar);
-    });
-
-
-
-
-    socket.on("restart_download", function(receiverId, receiverLabel){
-        //re init container
-        console.log("restart_download - "+receiverId);
-        $("#"+getRowId(receiverId)+" .receiver-label").html(receiverLabel+ " (experiencing network issues, download restarted)");
-        var transferProgressBar = $("#"+getTransfertBarId(receiverId));
-        transferProgressBar.attr('aria-valuenow', 0);
-        transferProgressBar.width('0%');
-        transferProgressBar.html('0%');
-        startUpload(fileToTransfert, receiverId,transferProgressBar);
-    });
-
-
-    //fonction d'upload du fichier
-    function startUpload(file, receiverId, transferProgressBar) {
-        console.log(file);
-
-        var readWriteOpts = {highWaterMark: Math.pow(2,21)};
-
-        $('#transfertMessage').html("Transfert in progress...");
-        var stream = ss.createStream(readWriteOpts);
-
-        // upload a file to the server.
-        ss(socket).emit('send_file', stream, receiverId,senderLabel);
-
-        var blobStream = ss.createBlobReadStream(file,readWriteOpts);
-        var size = 0;
-
-        blobStream.on('data', function (chunk) {
-            size += chunk.length;
-            //console.log(Math.floor(size / file.size * 100) + '%');
-            var progress = Math.floor(size / file.size * 100);
-
-            socket.emit('transfer_in_progress', progress, receiverId);
-
-            //update progress bar
-
-            transferProgressBar.attr('aria-valuenow', progress);
-            transferProgressBar.width(progress + '%');
-            transferProgressBar.html(progress + '%');
-        });
-
-
-        blobStream.pipe(stream);
-    }
-
-    socket.on('receiver_left', function(receiverId,receiverLabel){
-        var progressBarContainer = $('#'+getTransfertBarContainerId(receiverId));
-        progressBarContainer.empty();
-        progressBarContainer.append($("<p>").addClass("text-error").html("Receiver left before end of transfer"));
-        $("#transfert-"+receiverId+"-remove").show();
-        if(typeof receiverLabel != "undefined" && receiverLabel.length != 0 ) {
-            receiverLabel=receiverLabel+" "
-        }
-        jdNotif.notify("Something is wrong", "Apparently your friend "+receiverLabel+"left before the transfer was over");
-
-    });
-
-    socket.on('receiver_cancel_too_many_retries', function(receiverId,receiverLabel){
-        var progressBarContainer = $('#'+getTransfertBarContainerId(receiverId));
-        progressBarContainer.empty();
-        progressBarContainer.append($("<p>").addClass("text-error").html("Transfer canceled because of too many network failures"));
-        $("#transfert-"+receiverId+"-remove").show();
-        if(typeof receiverLabel != "undefined" && receiverLabel.length != 0 ) {
-            receiverLabel=receiverLabel+" "
-        }
-        jdNotif.notify("Something is wrong", "Apparently your friend "+receiverLabel+" had network failures, transfer has been canceled");
-
-    });
-
-    socket.on('transfer_complete', function(receiverId,receiverLabel){
-        console.log("transfer_complete - "+receiverId);
-        var progressBarContainer = $('#'+getTransfertBarContainerId(receiverId));
-        progressBarContainer.empty();
-        progressBarContainer.append($("<p>").addClass("text-success").html("File sent"));
-        $("#transfert-"+receiverId+"-remove").show();
-        if(typeof receiverLabel != "undefined" && receiverLabel.length != 0 ) {
-            receiverLabel=receiverLabel+" "
-        }
-        jdNotif.notify("Transfer complete", "your friend "+receiverLabel+"correctly received your file");
-    });
-
     $('#generatedurl').click(function () {
         $('#generatedurl').select();
     });
@@ -368,5 +225,28 @@ function sendFile(isLocal) {
         $('#generatedurlreminder').select();
     });
 
-
 }
+
+
+/******************************************************************/
+ReceiverInfo = function (label, progressBarContainer, progressBar, removeLinkContainer) {
+    this.init(label, progressBarContainer, progressBar, removeLinkContainer);
+};
+
+ReceiverInfo.prototype = {
+    constructor: ReceiverInfo,
+    init: function (label, progressBarContainer, progressBar, removeLinkContainer) {
+        this.label = label;
+        if (typeof label != "undefined" && label.length != 0) {
+            this.label = label + " "
+        }
+        this.progressBar = progressBar;
+        this.progressBarContainer = progressBarContainer;
+        this.removeLinkContainer = removeLinkContainer;
+
+        if (typeof this.label != "undefined" && this.label.length != 0) {
+            this.label = this.label + " "
+        }
+
+    }
+};
