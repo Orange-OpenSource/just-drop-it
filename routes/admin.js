c11c93359014b457c77918bfebf0fb7eb234cd3c
Arnaud Ruffin <arnaud.ruffin@orange.com>
2016-03-04 14:34:33 +0100

    Ajout des headers et licenses

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,23 @@
-/**
- * Created by buce8373 on 01/10/2015.
+/*
+ * just-drop-it
+ * Copyright (C) 2016 Orange
+ * Authors: Benjamin Einaudi  benjamin.einaudi@orange.com
+ *          Arnaud Ruffin arnaud.ruffin@orange.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 var express = require('express');
 var debug = require('debug')('app:routes:admin');
 var router = express.Router();
--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+/*
+ * just-drop-it
+ * Copyright (C) 2016 Orange
+ * Authors: Benjamin Einaudi  benjamin.einaudi@orange.com
+ *          Arnaud Ruffin arnaud.ruffin@orange.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #!/bin/env node
 
 "use strict";
@@ -22,7 +42,7 @@ debug.log = console.log.bind(console);
 
 var server = http.createServer(app);
 
-//retrieve Kermit variables
+//retrieve openshift variables
 var ipAddress = process.env.OPENSHIFT_NODEJS_IP || error('No OPENSHIFT_NODEJS_IP var, using ANY') || null;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
