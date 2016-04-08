1445d44a69533a1c86a75f52c61f1a0f32f6c92b
Ben Einaudi <benjamin.einaudi@orange.com>
2016-04-08 18:45:41 +0200

    Add boosted as a dependency This change add boosted as a dependency and expose its dist directory as a public directory Close #5

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, path.join('node_modules', 'boosted', 'dist'))));
 
 var receiveUriPath = '/receive';
 app.use('/', send);
