993e1f59bd14d08e8a1b7c6e4802c1e2ec055de9
Ben Einaudi <benjamin.einaudi@orange.com>
2016-04-08 11:53:25 +0200

    Add debug library To implement this feature, the library browserify was needed. Therefore the library socketio and debug are bundled as library that can be called from our code with require function. Removed all console.log Close #2

--- routes/javascript.js
+++ routes/javascript.js
@@ -22,45 +22,68 @@ var express = require('express');
 var read = require('fs').readFileSync;
 var uglifyJS = require("uglify-js");
 var router = express.Router();
+var browserify = require('browserify');
 var debug = require('debug')('app:routes:javascript');
 debug.log = console.log.bind(console);
 
 var servedFiles = {
-    "socket.io" : 'socket.io-client/socket.io.js',
-    "socket.io-stream" : 'socket.io-stream/socket.io-stream.js',
-    "jdropit-send" : '../client/jdropit-send.js',
-    "jdropit-receive" : '../client/jdropit-receive.js',
-    "jdropit-notif" : '../client/jdropit-notif.js',
-    "jdropit-whatsnew" : '../client/jdropit-whatsnew.js'
+    "jdropit-notif": '../client/jdropit-notif.js',
+    "jdropit-whatsnew": '../client/jdropit-whatsnew.js',
+    "jdropit-send": '../client/jdropit-send.js',
+    "jdropit-receive": '../client/jdropit-receive.js',
+    "socket.io-stream": 'socket.io-stream/socket.io-stream.js'
 };
+
+var commonLibraries = ['socket.io-client', 'debug'];
+
 
 var isLocal = typeof process.env.OPENSHIFT_NODEJS_IP === "undefined";
 
-function defineRoutesForLibrairy(libName){
-    var libDefinition = servedFiles[libName];
-    var source = read(require.resolve(libDefinition), 'utf-8');
+
+function routeLibrary(libName, sourceLoader, reloadOnLocal) {
+    var source = sourceLoader();
     var sourceMin = uglifyJS.minify(source, {fromString: true}).code;
-    router.get('/'+libName+'.js', function(req, res) {
+    router.get('/' + libName + '.js', function (req, res) {
         debug("providing %s", libName);
         res.setHeader('Content-Type', 'application/javascript');
-        if(isLocal) {//re read each time
-            res.status(200).end(read(require.resolve(libDefinition), 'utf-8'));
-        }else{
+        if (isLocal && reloadOnLocal) {//re read each time
+            res.status(200).end(sourceLoader());
+        } else {
             res.status(200).end(source);
         }
     });
-    router.get('/'+libName+'.min.js', function(req, res) {
+    router.get('/' + libName + '.min.js', function (req, res) {
         console.log("providing %s minified", libName);
         res.setHeader('Content-Type', 'application/javascript');
         res.status(200).end(sourceMin);
     });
-    debug("%s routed", libName);
 }
 
-for(var libName in servedFiles){
-    if(servedFiles.hasOwnProperty(libName)){
-        defineRoutesForLibrairy(libName);
+for (var libName in servedFiles) {
+    if (servedFiles.hasOwnProperty(libName)) {
+        routeLibrary(libName, (function (libName) {
+            return function () {
+                return read(require.resolve(servedFiles[libName]), 'utf-8');
+            }
+        })(libName), true);
     }
 }
+
+var browserifyEngine = browserify([], {basedir: __dirname});
+for (var cpt = 0; cpt < commonLibraries.length; cpt++){
+    browserifyEngine.require(commonLibraries[cpt]);
+}
+var stream = browserifyEngine.bundle();
+var fileData = [];
+stream.on('data', function (data) {
+    fileData.push(data);
+});
+stream.on('end', function () {
+    var data = fileData.join('');
+    routeLibrary('common-libraries', function(){
+       return data;
+    }, false);
+});
+
 
 module.exports = router;
